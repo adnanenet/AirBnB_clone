5b35eb81e422837e8c69b4325322a500daa367c0
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-12-11 13:45:24 +0000

    All tasks

--- models/base_model.py
+++ models/base_model.py
@@ -1,56 +1,58 @@
 #!/usr/bin/python3
-"""This script is the base model"""
-
-import uuid
-from datetime import datetime
+"""BaseModel that defines all common attributes/methods for other classes"""
+from uuid import uuid4
 from models import storage
+from datetime import datetime
 
 
 class BaseModel:
-
-    """Class from which all other classes will inherit"""
+    """"class BaseModel"""
+    date_format = '%Y-%m-%dT%H:%M:%S.%f'
 
     def __init__(self, *args, **kwargs):
-        """Initializes instance attributes
-
-        Args:
-            - *args: list of arguments
-            - **kwargs: dict of key-values arguments
-        """
-
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    self.__dict__[key] = kwargs[key]
+        """ initialization self"""
+        if kwargs:
+            kwargs['created_at'] = datetime.strptime(
+                kwargs['created_at'],
+                self.date_format)
+            if 'updated_at' in kwargs:
+                try:
+                    kwargs['updated_at'] = datetime.strptime(
+                        kwargs['updated_at'],
+                        self.date_format)
+                except BaseException:
+                    pass
+            if '__class__' in kwargs:
+                del kwargs['__class__']
+            self.__dict__ = kwargs
         else:
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
             storage.new(self)
 
-    def __str__(self):
-        """Returns official string representation"""
-
-        return "[{}] ({}) {}".\
-            format(type(self).__name__, self.id, self.__dict__)
-
     def save(self):
-        """updates the public instance attribute updated_at"""
-
+        """ updates public instance attribute with the current datetime """
         self.updated_at = datetime.now()
         storage.save()
 
-    def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__"""
+    def to_json(self):
+        """ returns a dict containing all keys\values of the __dict __ """
+        new_dict = self.__dict__.copy()
+        new_dict.update({'__class__': str(self.__class__.__name__)})
+        new_dict.update({'created_at': datetime.strftime(
+            (self.created_at), self.date_format)})
+        try:
+            new_dict.update({'updated_at': datetime.strftime(
+                (self.updated_at), self.date_format)})
+        except BaseException:
+            pass
+        return new_dict
 
-        my_dict = self.__dict__.copy()
-        my_dict["__class__"] = type(self).__name__
-        my_dict["created_at"] = my_dict["created_at"].isoformat()
-        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
-        return my_dict
+    def __str__(self):
+        """ printing string representation of BaseModel """
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
+
+    def __repr__(self):
+        """ format __str__ """
+        return self.__str__()
